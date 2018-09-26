948fabef342520ef499bd1f82d1cc4979cf40b2a
Jared Wilber <j.wilber92@gmail.com>
2018-09-25 23:38:00 -0700

    added barchart for first d3 + react example

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react';
 import Card from './card/Card';
 import Grid from './grid/Grid';
 import Graph from './graph/Graph';
+import BarChart from './barchart/Barchart';
 
 class App extends React.Component {
 
@@ -27,20 +28,24 @@ class App extends React.Component {
         </center>
         <br/>
         <br /> 
-        <Graph />
+        <div className="vis">
+        <BarChart data={[5,10,1,3]} size={[500,500]} />
+      </div>
         <br/>
         <div className="about">
               <p>Hello!  I’m a recent graduate of UC Berkeley interested in data science, open-source software, and information design.I’m currently a data scientist at Neudesic. Previously, I was on the data science team at Classy.org. Feel free to reach out to me via <a href='mailto:jdwlbr@gmail.com'>email</a>, <a href='https://twitter.com/jdwlbr'>Twitter</a>, or <a href='https://github.com/jwilber'>GitHub</a>. <br/><br/>
             </p>
+            
         </div>
         <hr className="hr_width"/>
           </center>
+
 
         <div className="p-4">
        
           <Grid settings={this.state} />
         </div>
-        <center><span style={{'font-size':'1.4rem','color':'#ccc'}}>
+        <center><span style={{'fontSize':'1.4rem','color':'#ccc'}}>
         ©2018 Jared Wilber | Made with React, Redux, & D3.js</span></center>
       </div>
     );
